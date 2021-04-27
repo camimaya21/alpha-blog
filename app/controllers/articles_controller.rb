2c1028fe781806750cdb9e45334b12d2a18acade
camimaya21 <camilamaya21@gmail.com>
2021-04-27 23:58:47 +0200

    add: form to create a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,12 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description, :status))
+    @article.save
+    redirect_to @article
+  end
 end
