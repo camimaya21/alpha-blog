c444a91a19e9685f84c8ed45e41259f81851d0a3
camimaya21 <camilamaya21@gmail.com>
2021-05-01 23:00:07 +0200

    add: edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = find_user
+  end
+
+  def update
+    @user = find_user
+    if @user.update(user_params)
+      flash[:notify] = "Your account was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def find_user
+    User.find(params[:id])
   end
   
 end
