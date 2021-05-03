869490de35b7f75a3f49343319e898bc34f3b778
Camila Maya S <31632069+camimaya21@users.noreply.github.com>
2021-05-03 22:49:12 +0200

    Feature/restrict routes when user is logged (#9)
    * add: Controller methods as helper methods
    
    * add: restrict actions from ui if user is logged_in?
    
    * add: modify navigation when user is logged_in?
    
    * add: restrict actions for articles controller
    
    * add: restrict actions for users controller
    
    * add: delete user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
   end
@@ -14,7 +16,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params_articles)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notify] = "Article was created succesfully"
       redirect_to @article
@@ -49,5 +51,12 @@ class ArticlesController < ApplicationController
 
   def params_articles
     params.require(:article).permit(:title, :description, :status)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
@@ -36,6 +38,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:current_user_id] = nil
+    flash[:notify] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -45,4 +54,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account"
+      redirect_to @user
+    end
+  end
+
 end
