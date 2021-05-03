bb8924ba53deb5916202c1c53c410007039f54c1
Camila Maya S <31632069+camimaya21@users.noreply.github.com>
2021-05-03 15:28:51 +0200

    add: pagination in index article, index user and show user profile (#7)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@ class UsersController < ApplicationController
   
   def show
     @user = find_user
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
